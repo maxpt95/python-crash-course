2f86b74481765cd506a04423dc70486aaba942f7
Maximiliano Pérez Tambasco <maxperez243@gmail.com>
2022-03-08 18:44:29 +0100

    Initial commit

---
+++ ch4-lists/ex3-8.py
@@ -0,0 +1,6 @@
+places = ["Venezuela","Denmark","Australia","USA","Domincan Republic"]
+
+#Print the list of places in reverse alphabetical order without modifying the list
+print( sorted(places,reverse=True) )
+
+print(places)
