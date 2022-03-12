1746777506173f47739251ddb9145be88ee70341
Maximiliano Pérez Tambasco <maxperez243@gmail.com>
2022-03-12 14:57:39 +0100

    chapter 4 and chapter 5 exercices
    chapter 4 - manipulating list: exercises and examples reviewed.
    chapter 5 - if statements: exercises

---
+++ ch4-lists/examples/rangeList.py
@@ -0,0 +1,4 @@
+# List from 10 to 20 made using range function
+rangeList = list(range(10, 21))
+
+print(rangeList)
---
+++ ch4-lists/excercises/ex4-9.py
@@ -0,0 +1,3 @@
+# Cube Comprehension: Use a list comprehension to generate a list of thefirst 10 cubes.
+cubes = [value**3 for value in range(1, 11)]
+print(cubes)
