9c5296c2de438e7366f15d723b84aa211a1f4a84
Maximiliano Pérez Tambasco <maxperez243@gmail.com>
2022-03-16 11:35:58 +0100

    Chapter 7 - Examples

---
+++ ch7-inputs-and-whileloops/examples/input.py
@@ -0,0 +1,5 @@
+inputTest = input("This is an imput test for VScode: ")
+
+testResult = "VScode python interpreter can take inputs and print them."
+testResult += f" Here is your input!\n{inputTest}"
+print(testResult)
